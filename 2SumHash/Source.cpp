dbb09bf219b9829f7dd5be8baa96dc35d3cd00e0
AdamBJ <anb1@sfu.ca>
2014-11-25 15:53:56 +0900

    final, debug code removed

--- 2SumHash/Source.cpp
+++ 2SumHash/Source.cpp
@@ -1,24 +1,12 @@
 #include <unordered_map>
 #include <fstream>
 #include <iostream>
-#include <sstream>
-#include <vector>
 
-const std::string FILE_NAME = "test.txt";
+const std::string FILE_NAME = "2sum.txt";
 
 int main(){
 	std::unordered_map < long long, long long> hashTable;
-	//hashTable.reserve(1000000);//reserve creates a number of buckets in the hashtable most appropriate for "argument" number of elements.
-
-	//std::unordered_map < long long, long long> hashTable2;
-
-	//hashTable2[1] = 2;
-	//auto u = hashTable2.find(1);
-	//auto p =hashTable2.find(2);
-
-	//if (p == hashTable2.end()){
-	//	std::cout<< "ppepe";
-	//}
+	hashTable.reserve(1000000);//reserve creates a number of buckets in the hashtable most appropriate for "argument" number of elements.
 
 	std::ifstream integersToBeRead(FILE_NAME);
 
@@ -37,23 +25,16 @@ int main(){
 	std::vector<std::string> u;
 
 	while (t <= 10000){
-		for (auto it = hashTable.begin(); it != hashTable.end(); ++it){//use auto& if you want to be able to modify the values you're iterating over
-			//std::cout << " " << it->first << ":" << it->second << std::endl; this is how you print out the elements of a hash table
-			
+		for (auto it = hashTable.begin(); it != hashTable.end(); ++it){
 			x = it->second; //get the key stored at the current position
 			y = t - x; //because x+y=t, t-x =y;
 			if (hashTable.find(y) != hashTable.end() && x!=y){//find returns hashTable.end if the key doesn't exist.
 				hitCount++;
-				std::stringstream ss;
-				ss << x << " " << y << " " << t;
-				u.push_back(ss.str());
-
 				break;
 			}
 		}
 		t++;
 	}
-
 	std::cout << hitCount;
 	return 0;
 }
